49a18ec76ac762f0adaeea944c3a561b7878f357
carlos sanchez bullio <c87mp@hotmail.com>
2020-02-03 23:01:13 -0600

    update3

---
+++ UAM.OOP/Tarea/Netflix.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAM.OOP
+{
+    public class Netflix : reproductores
+    {
+
+        private int id;
+        private String nombre;
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+
+
+        public Netflix() {
+
+
+        }
+
+
+
+        public Netflix(int id, string nombre)
+        {
+            this.id = id;
+            this.nombre = nombre;
+        }
+
+        
+
+
+
+        public void forward()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void play()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void rewind()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UAM.OOP/Tarea/reproductores.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAM.OOP
+{
+    interface reproductores
+
+    {
+
+        void play();
+
+        void stop();
+
+        void forward();
+
+        void rewind();
+
+
+    }
+}
